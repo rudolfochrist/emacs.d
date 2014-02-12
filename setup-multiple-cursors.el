f3b01ef6b82ba1996956f372af1d03ebb37e5b2b
Sebastian Christ <rudolfo.christ@gmail.com>
2014-02-12 20:17:14 +0100

    change keybindings for multiple cursors.

--- setup-multiple-cursors.el
+++ setup-multiple-cursors.el
@@ -1,8 +1,8 @@
 (require-package 'multiple-cursors)
 
-(global-set-key (kbd "C-c C-l") 'mc/edit-lines)
-(global-set-key (kbd "M-}") 'mc/mark-next-like-this)
-(global-set-key (kbd "M-{") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-S-}") 'mc/mark-all-like-this)
 
 (provide 'setup-multiple-cursors)
