3b2d35dbe14c6d15469a3cffc1a191ae4e99cd68
Sebastian Christ <rudolfo.christ@gmail.com>
2014-04-09 22:58:12 +0200

    Replace js3-mode with js2-mode and add js-comint

--- setup-javascript.el
+++ setup-javascript.el
@@ -1,6 +1,23 @@
-(require-package 'js3-mode)
+(require-package 'js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
+;;; REPL glory
+(require-package 'js-comint)
+(setq inferior-js-program-command "/usr/local/bin/node")
+(setenv "NODE_NO_READLINE" "1")
+
+(defun fyi-js2-mode-keys () 
+  "My js3-mode key bindings"
+  (interactive)
+  (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+  (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
+  (local-set-key (kbd "C-c C-b") 'js-send-buffer)
+  (local-set-key (kbd "C-c C-k") 'js-send-buffer-and-go)
+  (local-set-key (kbd "C-c l") 'js-load-file-and-go))
+
+(add-hook 'js2-mode-hook 'fyi-js2-mode-keys)
+
 
 (provide 'setup-javascript)
