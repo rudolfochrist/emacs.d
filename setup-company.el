241713e815e990e1dc43e5fb44d5d9d2ada7b439
Sebastian Christ <rudolfo.christ@gmail.com>
2014-06-05 22:31:01 +0200

    Rebind company complete to control space.

--- setup-company.el
+++ setup-company.el
@@ -3,6 +3,6 @@
 
 (setq company-idle-delay nil)           ; disable time delay
 (add-hook 'after-init-hook 'global-company-mode) ; enable company in all modes
-(global-set-key (kbd "TAB") 'company-complete)
+(global-set-key (kbd "C-SPC") 'company-complete)
 
 (provide 'setup-company)
