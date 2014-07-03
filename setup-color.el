ffba751be448ea941b9702b218ae782393c9f739
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-03 14:34:12 +0200

    Adjust size of org mode level 1 heading.

--- setup-color.el
+++ setup-color.el
@@ -15,7 +15,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Ubuntu_Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Ubuntu_Mono"))))
+ '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.2)))))
 
 ;; when in GUI emacs set size of frame
 (when window-system
