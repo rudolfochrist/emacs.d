125cbd50844bcd589fc6f1031550b6105b78bee4
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-09 11:15:04 +0200

    Use electric help

--- setup-keybindings.el
+++ setup-keybindings.el
@@ -6,4 +6,9 @@
 ;; revert buffer
 (global-set-key (kbd "C-c r") 'revert-buffer)
 
+;;; use ElectricHelp as default
+;;; see: http://www.emacswiki.org/emacs/ElectricHelp
+(require 'ehelp)
+(global-set-key (kbd "C-h") 'ehelp-command)
+
 (provide 'setup-keybindings)
