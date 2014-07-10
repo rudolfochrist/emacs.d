019ae055aa3d56f63902f1b1615791f2487aa9ab
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-10 22:12:49 +0200

    Starting a Zettelkasten in org

--- setup-org-mode.el
+++ setup-org-mode.el
@@ -12,5 +12,30 @@
           (lambda ()
             (visual-line-mode t)
             (set-input-method 'german-prefix)))
+(setq org-agenda-files '("~/Documents/todo.org"
+                         "~/Documents/calendar.org"))
+
+(defun add-zettle ()
+  (interactive)
+  (org-insert-heading)
+  (insert (generate-zettel-id) " "))
+
+(defun generate-zettel-id ()
+  (time-fragments->string (extract-time-fragments (decode-time))))
+
+(defun time-fragments->string (time-fragments)
+  (reduce (lambda (m o)
+            (concat m (number-to-string o)))
+          time-fragments :initial-value ""))
+
+(defun extract-time-fragments (decoded-time)
+  (reverse (mapcar (lambda (idx)
+             (nth idx decoded-time))
+           '(1 2 3 4 5))))
+
+(defun fyi/org-mode-keybindings ()
+  (local-set-key (kbd "C-c z") 'add-zettle))
+
+(add-hook 'org-mode-hook 'fyi/org-mode-keybindings)
 
 (provide 'setup-org-mode)
