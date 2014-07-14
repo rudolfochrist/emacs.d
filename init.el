b84c55f11550e3dec97d280fe3347eeeca5a6db5
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-14 15:43:54 +0200

    Adjust font size for org headings

--- init.el
+++ init.el
@@ -62,3 +62,17 @@
 (dolist (file setup-pkg-full)
   (require file))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Ubuntu_Mono"))))
+ '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
+ '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0)))))
--- setup-color.el
+++ setup-color.el
@@ -4,20 +4,6 @@
 
 (load-theme 'spolsky t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Ubuntu_Mono"))))
- '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.2)))))
-
 ;; when in GUI emacs set size of frame
 (when window-system
   (set-frame-size (selected-frame) 150 45))
