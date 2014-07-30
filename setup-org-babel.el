ed61e5000e55f3a506a994313d515a61d43770bb
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-30 14:21:44 +0200

    Org-Babel customization for literate programming

--- setup-org-babel.el
+++ setup-org-babel.el
@@ -12,6 +12,9 @@
                                                          (sh . t)
                                                          ))
 
-(setq org-confirm-babel-evaluate nil)
+(setq org-confirm-babel-evaluate nil)   ; Don't ask for permission
+(setq org-src-fontify-natively t)       ; Show syntax highlighting per language native mode in *.org
+(setq org-src-preserve-indentation t)   ; For languages with significant whitespace like Python
+
 
 (provide 'setup-org-babel)
