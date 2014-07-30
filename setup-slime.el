054f5a7f279b8058cb24937f5e8ee745b40a4217
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-30 14:21:01 +0200

    Don't load slime in interaction mode
    Give back evaluation of Emacs Lisp in *scratch* buffer

--- setup-slime.el
+++ setup-slime.el
@@ -2,8 +2,6 @@
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
 (add-hook 'lisp-mode-hook #'slime-mode)
-(add-hook 'lisp-interaction-mode-hook #'slime-mode)
-
 (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbols)
 
 (provide 'setup-slime)
