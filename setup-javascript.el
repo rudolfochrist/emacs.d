4852cc2f9f8a7253619bfd8c0a804c54a97a145f
Sebastian Christ <rudolfo.christ@gmail.com>
2014-07-31 09:42:01 +0200

    And again. New Javascript...

--- setup-javascript.el
+++ setup-javascript.el
@@ -1,10 +1,20 @@
-(require-package 'js2-mode)
+(require-package 'js3-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js3-mode))
 
-(require-package 'skewer-mode)
-(setq phantomjs-program-name "/usr/local/bin/phantomjs")
-(skewer-setup)
+(require-package 'js-comint)
+
+(setenv "NODE_DISABLE_COLORS" "1")
+(setenv "NODE_NO_READLINE" "1")
+(setq inferior-js-program-command "/usr/local/bin/node")
+
+(defun fyi/js-key-bindings ()
+  (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+  (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
+  (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go)
+  (local-set-key (kbd "C-c C-l") 'js-load-file-and-go))
+
+(add-hook 'js3-mode-hook 'fyi/js-key-bindings)
 
 (provide 'setup-javascript)
