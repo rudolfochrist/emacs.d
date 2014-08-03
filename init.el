413dedf7ab5269f91ee1f6379e5b02955ad86ffa
Sebastian Christ <rudolfo.christ@gmail.com>
2014-08-03 19:10:34 +0200

    Change font

--- init.el
+++ init.el
@@ -74,6 +74,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Ubuntu_Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "gray13" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "PT_Mono"))))
  '(org-level-1 ((t (:foreground "dodger blue" :weight bold :height 1.0))))
  '(org-level-2 ((t (:foreground "#edd400" :weight bold :height 1.0)))))
