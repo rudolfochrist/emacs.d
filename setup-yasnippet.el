73b5557d7cee52989f2d2c8e07484ccbea19c8b0
Sebastian Christ <rudolfo.christ@gmail.com>
2014-09-03 11:52:26 +0200

    Add enable yasnippets in more modes

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -1,7 +1,13 @@
 (require-package 'yasnippet)
 
 (yas-reload-all)
-(add-hook 'prog-mode-hook '(lambda ()
-                             (yas-minor-mode)))
+
+(defun enable-yas-minor-mode ()
+  (yas-minor-mode))
+
+(add-hook 'prog-mode-hook 'enable-yas-minor-mode)
+(add-hook 'html-mode-hook 'enable-yas-minor-mode)
+(add-hook 'web-mode-hook 'enable-yas-minor-mode)
+(add-hook 'org-mode-hook 'enable-yas-minor-mode)
 
 (provide 'setup-yasnippet)
