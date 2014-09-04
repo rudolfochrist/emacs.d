9d230cc6547efe9f9c798b6c67795c6bda54eeeb
Sebastian Christ <rudolfo.christ@gmail.com>
2014-09-04 14:58:44 +0200

    Use UTF-8

--- setup-slime.el
+++ setup-slime.el
@@ -4,4 +4,7 @@
 (add-hook 'lisp-mode-hook #'slime-mode)
 (setq slime-complete-symbol-function #'slime-fuzzy-complete-symbol)
 
+;;; use UTF-8
+(setq slime-net-coding-system 'utf-8-unix)
+
 (provide 'setup-slime)
