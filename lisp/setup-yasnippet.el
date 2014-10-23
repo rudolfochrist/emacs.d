030c3f6266351490db3836145ffc6b7781b3cf8d
Sebastian Christ <rudolfo.christ@gmail.com>
2014-10-23 07:42:28 +0200

    Fix yasnippet tab key in org-mode

--- lisp/setup-yasnippet.el
+++ lisp/setup-yasnippet.el
@@ -8,6 +8,16 @@
 (add-hook 'prog-mode-hook 'enable-yas-minor-mode)
 (add-hook 'html-mode-hook 'enable-yas-minor-mode)
 (add-hook 'web-mode-hook 'enable-yas-minor-mode)
-(add-hook 'org-mode-hook 'enable-yas-minor-mode)
+
+;;; fix org-mode + yasnippet conflicts:
+;;; http://stackoverflow.com/questions/9418148/conflicts-between-org-mode-and-yasnippet
+(defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+(add-hook 'org-mode-hook (lambda ()
+                           (make-variable-buffer-local 'yas/trigger-key)
+                           (setq yas/trigger-key [tab}])
+                           (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+                           (define-key yas/keymap [tab] 'yas/next-field)))
 
 (provide 'setup-yasnippet)
