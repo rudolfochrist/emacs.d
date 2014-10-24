139cffc958e6d1fcfa49bf0bba13c27d482e1909
Sebastian Christ <rudolfo.christ@gmail.com>
2014-10-24 15:14:42 +0200

    Use http repo

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 
 ;; Packages
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")
