7dc36068b920c46d29668c9aad9f5e2cdce75667
Sebastian Christ <rudolfo.christ@gmail.com>
2014-11-04 10:24:20 +0100

    add new melpa URL

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("sunrise-commander" . "http://joseito.republika.pl/sunrise-commander/")))
 (package-initialize)
