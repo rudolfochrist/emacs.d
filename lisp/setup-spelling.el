f48299d25ccb8ebda0b0d1412f18c96b4ccf703b
Sebastian Christ <rudolfo.christ@gmail.com>
2014-12-20 21:37:01 +0100

    Don't automatically correct spelling in buffers.

--- lisp/setup-spelling.el
+++ lisp/setup-spelling.el
@@ -2,7 +2,4 @@
 (setq ispell-list-command "list")
 (setq ispell-extra-args '("--sug-mode=ultra"))
 
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
-
 (provide 'setup-spelling)
