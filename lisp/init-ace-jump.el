03d3388ce03378ed01b78afe7e555b08ff13ad3b
Sebastian Christ <rudolfo.christ@gmail.com>
2015-03-27 08:25:15 +0100

    customizing ace-jump

--- lisp/init-ace-jump.el
+++ lisp/init-ace-jump.el
@@ -1,5 +1,12 @@
 (require-package 'ace-jump-mode)
 
+(setq ace-jump-mode-scope 'frame)
+
+(setq ace-jump-mode-submode-list
+     '(ace-jump-word-mode
+       ace-jump-line-mode
+       ace-jump-char-mode))
+
 (global-set-key (kbd "C-M-S-q") 'ace-jump-mode)
 
 (provide 'init-ace-jump)
