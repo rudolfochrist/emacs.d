6e575a4545a5403f5257cbfd7a260d0f1dfbe58a
Sebastian Christ <rudolfo.christ@gmail.com>
2015-04-12 12:24:30 +0200

    `check-parens' after save in lisp buffers

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -1,3 +1,9 @@
 
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+
+(defun fyi/lisp-after-save ()
+  (when (derived-mode-p 'emacs-lisp-mode 'lisp-mode)
+    (check-parens)))
+(add-hook 'after-save-hook #'fyi/lisp-after-save)
+
 (provide 'init-emacs-lisp)
