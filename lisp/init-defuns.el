152eb7dbe71102ec985a01928ba2baba7784d5f2
Sebastian Christ <rudolfo.christ@gmail.com>
2015-04-16 12:29:55 +0200

    Add docs

--- lisp/init-defuns.el
+++ lisp/init-defuns.el
@@ -1,20 +1,27 @@
+;;; init-defuns.el --- utility functions
+;;; Code:
+
 (defun fyi/format-json ()
+  "Pretty format json."
   (interactive)
   (shell-command-on-region
    (mark) (point) "python -m json.tool" (buffer-name) t))
 
 (defun fyi/format-xml ()
+  "Pretty format xml."
   (interactive)
   (shell-command-on-region
    (mark) (point) "xmllint --format -" (buffer-name) t))
 
 (defun fyi/configure-flyspell (dictionary)
-  "Applies flyspell settings"
+  "Apply flyspell settings.
+Argument DICTIONARY The Ispell dictionary to use."
   (ispell-change-dictionary dictionary)
   (set-input-method "german-prefix")
   (flyspell-mode 1))
 
 (defun fyi/search-kb (search-string)
+  "Search the KB for SEARCH-STRING."
   (interactive "MSearch String: ")
   (grep (concat "grep --color -inH -C 3 -E "
                 (shell-quote-argument search-string)
@@ -24,7 +31,7 @@
 (global-set-key (kbd "C-c C-c p") 'fyi/search-kb)
 
 (defun fyi/enumerate-list (lst)
-  "Transform LIST to an enumerated alist.
+  "Transform LST to an enumerated alist.
 
 Example:
 '(x y) gets transformed to '((x . 1) (y . 2))"
@@ -37,14 +44,36 @@ Example:
   "Generate a date-based revision string."
   (format-time-string format-string))
 
-(defun fyi/update-version-string (version-alist &optional create-tag-p)
+(defun fyi/update-version-string (version-values &optional create-tag-p)
+  "Update a version at point.
+
+The version string must have the MAJOR.MINOR.PATCH format.
+
+The VERSION-VALUES has the form
+'((\"0\" . 1) (\"12\" . 2) (\"0\" . 3))
+
+where the `car' is the new value and the `cdr' is the positon. The position
+is mapped with
+
+MAJOR = 1
+MINOR = 2
+PATCH = 3
+
+When called interactivley than only the PATH gets updated with the value
+provided by `fyi/date-revision'.
+
+When called interactively with a prefix-argument the values must
+entered in the minibuffer.
+
+If CREATE-TAG-P is non-nil than the current file gets saved, committed
+and a tag is generated."
   (interactive
    (cond
      ((equal current-prefix-arg '(4))
       (let ((major (read-string "Major: "))
             (minor (read-string "Minor: "))
             (patch (read-string "Patch: "))
-            (git-tag (y-or-n-p "Create tag?")))
+            (git-tag (y-or-n-p "Create tag? ")))
         (list (fyi/enumerate-list (list major minor patch))
               git-tag)))
      (t
@@ -54,7 +83,7 @@ Example:
   (when (match-string 0)
     (mapc (lambda (version)
             (replace-match (car version) nil nil nil (cdr version)))
-          version-alist)
+          version-values)
     (when create-tag-p
       (let ((new-version (thing-at-point 'symbol)))
         (save-buffer)
@@ -64,3 +93,5 @@ Example:
         (magit-tag (format "v%s" new-version) "HEAD")))))
 
 (provide 'init-defuns)
+
+;;; init-defuns.el ends here
