edb86ec4dc49a9199d2785c83d97ea070a4251c3
Sebastian Christ <rudolfo.christ@gmail.com>
2015-04-17 08:12:26 +0200

    Limit ace-window to the current frame

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -1,5 +1,7 @@
 (require-package 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
+      aw-scope 'frame)
 (ace-window-display-mode 1)
 
 (global-set-key (kbd "C-x o") 'ace-window)
