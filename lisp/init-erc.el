925431b59f789e782b11eec6d905f195e4fbb792
Sebastian Christ <rudolfo.christ@gmail.com>
2015-04-17 08:12:50 +0200

    Don't show join, part and quit messages in erc

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -3,6 +3,7 @@
 (setq erc-nick "rudolfochrist"
       erc-prompt-for-password nil
       erc-user-full-name "Sebastian (Rudolfo) Christ"
-      erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#lisp" "#lispcafe")))
+      erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#lisp" "#lispcafe"))
+      erc-hide-list '("JOIN" "PART" "QUIT"))
 
 (provide 'init-erc)
