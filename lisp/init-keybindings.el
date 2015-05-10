7df3023b8ea5342f53b6224960d1882a2769e189
Sebastian Christ <rudolfo.christ@gmail.com>
2015-05-10 10:10:20 +0200

    rebind M-x to single key

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -8,10 +8,7 @@
 ;;; always use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;;; always do regex search
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+;;; single M-x key
+(global-set-key (kbd "C-M-w") 'execute-extended-command)
 
 (provide 'init-keybindings)
