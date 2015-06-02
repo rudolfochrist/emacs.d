eec4e353ce451eadee2fbbfcc81179f81114f621
Sebastian Christ <rudolfo.christ@gmail.com>
2015-06-02 12:44:33 +0200

    add hydra for paredit

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -11,7 +11,36 @@
   (define-key paredit-mode-map (kbd "[") 'paredit-open-round)
   (define-key paredit-mode-map (kbd "]") 'paredit-close-round)
   (define-key paredit-mode-map (kbd "(") 'paredit-open-bracket)
-  (define-key paredit-mode-map (kbd ")") 'paredit-close-bracket))
+  (define-key paredit-mode-map (kbd ")") 'paredit-close-bracket)
+
+  (define-key paredit-mode-map
+      (kbd "qq")
+    (defhydra hydra-paredit (:hint nil
+                             :idle )
+      "
+^Movement & Navigation^
+-----------------------
+_h_: backward sexp   _H_: previous line
+_j_: forward down    _J_: backward down    _a_: beginning of sexp
+_k_: backward up     _K_: forward up       _e_: end of sexp
+_l_: forward sexp    _L_: next line
+"
+      ("h" paredit-backward)
+      ("j" paredit-forward-down)
+      ("k" paredit-backward-up)
+      ("l" paredit-forward)
+      ("H" previous-line)
+      ("J" paredit-backward-down)
+      ("K" paredit-forward-up)
+      ("L" next-line)
+      ("a" (lambda ()
+             (interactive)
+             (paredit-backward-up)
+             (forward-char)))
+      ("e" (lambda ()
+             (interactive)
+             (paredit-forward-up)
+             (backward-char))))))
 
 ;;; use `common-lisp-indent-function'
 (setq lisp-indent-function #'common-lisp-indent-function)
