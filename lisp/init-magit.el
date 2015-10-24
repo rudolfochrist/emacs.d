7e4e8ed793f841d58494be20818d75cef751ce3c
Sebastian Christ <rudolfo.christ@gmail.com>
2015-10-24 15:05:07 +0200

    Add magit-gh-pulls extension.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,9 @@
 (require-package 'magit)
 (require-package 'git-timemachine)
 
+(require-package 'magit-gh-pulls)
+(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+
 ;; magit status
 (global-set-key (kbd "<f9>") 'magit-status)
 
