0fce8426ae5c5f209032f62cecbf3c681d099ae9
Sebastian Christ <rudolfo.christ@gmail.com>
2015-11-02 20:45:22 +0100

    Add smart-mode-line.

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -31,4 +31,27 @@
       show-paren-style 'mixed)
 (show-paren-mode 1)
 
+;;; mode-line globals
+(setq display-time-day-and-date t
+      display-time-24hr-format t)
+(display-time-mode 1)
+(display-battery-mode 1)
+
+;;; which-function-mode
+(which-func-mode 1)
+
+;;; smart-mode-line
+(require-package 'rich-minority)
+(require-package 'smart-mode-line)
+(defvar *rm-mode-regexps*
+  '(" \\[.*\\]"                         ; slime package + CL implementaion
+    )
+  "Regular expressions to minor modes visible in the mode line.")
+(setq rm-whitelist (mapconcat 'identity *rm-mode-regexps* "\\|"))
+(sml/setup)
+
+;;; face adjustments
+(custom-set-faces
+ '(which-func ((t :foreground "white"))))
+
 (provide 'init-appearance)
