c32d9cd33e9d6390f7e9b794429628c7952cc62d
Sebastian Christ <rudolfo.christ@gmail.com>
2015-12-12 21:42:21 +0100

    ~C-return~ for `other-window'.

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -3,8 +3,16 @@
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
       aw-dispatch-always t)
 (ace-window-display-mode 1)
+(global-set-key (kbd "C-x o") #'ace-window)
 
-(global-set-key (kbd "C-x o") 'ace-window)
-(global-set-key (kbd "C-x p") 'aw-flip-window)
+(defun fyi-switch-windows ()
+  (interactive)
+  (let ((window (aw--pop-window)))
+    (if window
+        (aw-switch-to-window window)
+      (other-window 1))))
+
+(when window-system
+  (global-set-key (kbd "<C-return>") #'fyi-switch-windows))
 
 (provide 'init-ace-window)
