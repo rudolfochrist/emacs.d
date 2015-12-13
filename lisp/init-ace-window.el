23d5f9a97c0dc035e2a2cec706bac1d3417bba50
Sebastian Christ <rudolfo.christ@gmail.com>
2015-12-13 17:50:39 +0100

    Just use `other-window'.

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -5,14 +5,7 @@
 (ace-window-display-mode 1)
 (global-set-key (kbd "C-x o") #'ace-window)
 
-(defun fyi-switch-windows ()
-  (interactive)
-  (let ((window (aw--pop-window)))
-    (if window
-        (aw-switch-to-window window)
-      (other-window 1))))
-
 (when window-system
-  (global-set-key (kbd "<C-return>") #'fyi-switch-windows))
+  (global-set-key (kbd "<C-return>") #'other-window))
 
 (provide 'init-ace-window)
