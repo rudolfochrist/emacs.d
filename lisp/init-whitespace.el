fdadd1aac68fae62ee2f71670862d8ef852ea241
Sebastian Christ <rudolfo.christ@gmail.com>
2015-12-27 15:51:40 +0100

    Increase whitspace to 120.

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -1,5 +1,6 @@
 (require 'whitespace)
-(setq whitespace-style '(face lines-tail))
+(setq whitespace-line-column 120
+      whitespace-style '(face lines-tail))
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
 (provide 'init-whitespace)
