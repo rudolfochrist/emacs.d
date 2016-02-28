35bd26b678ae23bc101084c45cbcee9e52c24d92
Sebastian Christ <rudolfo.christ@gmail.com>
2016-02-28 13:07:08 +0100

    Async copy files in dired with rsync.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -67,4 +67,37 @@
 (setq dired-details-hidden-string "--- ")
 (dired-details-install)
 
+;;; async copy many files
+;;; http://oremacs.com/2016/02/24/dired-rsync/
+(defun fyi-dired-rsync (dest)
+  (interactive
+   (list
+    (expand-file-name
+     (read-file-name
+      "Rsync to:"
+      (dired-dwim-target-directory)))))
+  ;; store all selected files into "files" list
+  (let ((files (dired-get-marked-files
+                nil current-prefix-arg))
+        ;; the rsync command
+        (tmtxt/rsync-command
+         "rsync -arvz --progress "))
+    ;; add all selected file names as arguments
+    ;; to the rsync command
+    (dolist (file files)
+      (setq tmtxt/rsync-command
+            (concat tmtxt/rsync-command
+                    (shell-quote-argument file)
+                    " ")))
+    ;; append the destination
+    (setq tmtxt/rsync-command
+          (concat tmtxt/rsync-command
+                  (shell-quote-argument dest)))
+    ;; run the async shell command
+    (async-shell-command tmtxt/rsync-command "*rsync*")
+    ;; finally, switch to that window
+    (other-window 1)))
+
+(define-key dired-mode-map "Y" 'fyi-dired-rsync)
+
 (provide 'init-dired)
