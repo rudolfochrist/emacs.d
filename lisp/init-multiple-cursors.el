da41b2b6b57cf7dbe9a9d0512f57f7f1c7c53bbc
Sebastian Christ <rudolfo.christ@gmail.com>
2016-03-30 14:56:40 +0200

    Extend multiple-cursors hydra.

--- lisp/init-multiple-cursors.el
+++ lisp/init-multiple-cursors.el
@@ -9,6 +9,9 @@
 [_p_]   Next    [_n_]   Next    [_l_] Edit lines
 [_P_]   Skip    [_N_]   Skip    [_a_] Mark all
 [_M-p_] Unmark  [_M-n_] Unmark  [_q_] Quit
+
+[_i_] Insert numbers \(optional number prefix\)
+[_I_] Insert letter \(optinoal number prefix\)
 "
   ("l" mc/edit-lines :exit t)
   ("a" mc/mark-all-like-this :exit t)
@@ -18,6 +21,8 @@
   ("p" mc/mark-previous-like-this)
   ("P" mc/skip-to-previous-like-this)
   ("M-p" mc/unmark-previous-like-this)
+("i" mc/insert-numbers)
+("I" mc/insert-letters)
   ("q" nil)))
 
 (provide 'init-multiple-cursors)
