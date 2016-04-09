bccc15b4f576d4b1645e6d78dfab47bcd206f70b
Sebastian Christ <rudolfo.christ@gmail.com>
2016-04-09 12:34:28 +0200

    Add new WIP

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -27,7 +27,7 @@
 
 ;;; [[http://endlessparentheses.com/visit-directory-inside-a-set-of-directories.html][Visit Directory inside a Set of Directories with Emacs]]
 (defcustom fyi-favorite-directories
-  '("~/dev/" "~/wip/" "~/Documents/Archive/" "~/.emacs.d/" "~/quicklisp/local-projects/"
+  '("~/dev/" "~/wip/" "~/Dropbox/wip/" "~/Documents/Archive/" "~/.emacs.d/" "~/quicklisp/local-projects/"
     "~/Dropbox/Lectures/HS-Mannheim/" "~/Documents/workspace/")
   "List of favorite directories.
 Used in `fyi-visit-favorite-dir'. The order here
