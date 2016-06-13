b8b67aaf41e704ba799d3034179cb49d5c955732
Sebastian Christ <rudolfo.christ@gmail.com>
2016-06-13 11:42:45 +0200

    Update packages.

--- elpa/ivy-20160606.2321/ivy-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "ivy" "20160606.2321" "Incremental Vertical completYon"
-  '((emacs "24.1"))
-  :url "https://github.com/abo-abo/swiper" :keywords
-  '("matching"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
