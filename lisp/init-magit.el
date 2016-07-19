cc6267d3f7214d0fcfd19f9e19a33c8767b51262
Sebastian Christ <rudolfo.christ@gmail.com>
2016-07-19 20:42:47 +0200

    Remove `magit-gh-pulls'.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,9 +1,6 @@
 (require-package 'magit)
 (require-package 'git-timemachine)
 
-(require-package 'magit-gh-pulls)
-(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
-
 ;; magit status
 (global-set-key (kbd "<f9>") 'magit-status)
 
