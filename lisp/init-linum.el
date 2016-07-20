92b8363560fad08c79b2addbe9901d54d4ccd5d8
Sebastian Christ <rudolfo.christ@gmail.com>
2016-07-20 11:30:07 +0200

    Better structure.

--- lisp/init-linum.el
+++ lisp/init-linum.el
@@ -5,6 +5,12 @@
 ;;; disable linum in some major modes
 (require-package 'linum-off
                  :from-dir emacs-d-site-lisp)
-(setq linum-disabled-modes-list '(eshell-mode wl-summary-mode compilation-mode dired-mode doc-view-mode image-mode))
+(setq linum-disabled-modes-list
+      '(eshell-mode
+        wl-summary-mode
+        compilation-mode
+        dired-mode
+        doc-view-mode
+        image-mode))
 
 (provide 'init-linum)
