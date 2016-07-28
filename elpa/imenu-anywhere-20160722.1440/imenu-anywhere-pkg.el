b6e985dfed4c387524f12010cf526f2be63dcd92
Sebastian Christ <rudolfo.christ@gmail.com>
2016-07-28 10:30:56 +0200

    Update packages

---
+++ elpa/imenu-anywhere-20160722.1440/imenu-anywhere-pkg.el
@@ -0,0 +1,1 @@
+(define-package "imenu-anywhere" "20160722.1440" "ido/ivy/helm imenu across same mode/project/etc buffers" '((cl-lib "0.5")) :url "https://github.com/vitoshka/imenu-anywhere" :keywords '("ido" "imenu" "tags"))
--- elpa/rich-minority-20160701.1144/rich-minority-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rich-minority" "20160701.1144" "Clean-up and Beautify the list of minor-modes." '((cl-lib "0.5")) :url "https://github.com/Malabarba/rich-minority" :keywords '("mode-line" "faces"))
