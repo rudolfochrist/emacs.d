c941836f310981643457f5cc31dbb64b4f88086b
Sebastian Christ <rudolfo.christ@gmail.com>
2016-08-01 17:16:11 +0200

    Prevent compile errors on emacs25.

--- lisp/init-eval-sexp-fu.el
+++ lisp/init-eval-sexp-fu.el
@@ -1,4 +1,8 @@
-(require-package 'eval-sexp-fu)
+(let ((byte-compile-not-obsolete-funcs
+       (append byte-compile-not-obsolete-funcs
+               (when (>= emacs-major-version 25)
+                 (list 'preceding-sexp)))))
+  (require-package 'eval-sexp-fu))
 
 (add-hook 'lisp-mode-hook #'(lambda ()
                               (eval-sexp-fu-flash-mode 1)))
