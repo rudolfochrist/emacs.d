87b29a2789a8c18ede3898fbde50d63f8826f2b8
Sebastian Christ <rudolfo.christ@gmail.com>
2016-09-13 09:26:28 +0200

    update packages.

--- elpa/zerodark-theme-20160820.750/zerodark-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zerodark-theme" "20160820.750" "A dark, medium contrast theme for Emacs" 'nil :url "https://github.com/NicolasPetton/zerodark-theme" :keywords '("themes"))
