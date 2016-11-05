14749d619053e23ab286b4785fb98071d18a9385
Sebastian Christ <rudolfo.christ@gmail.com>
2016-11-05 09:55:24 +0100

    Update packages

--- elpa/highlight-20160617.617/highlight-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "highlight" "20160617.617" "Highlighting commands." 'nil :url "http://www.emacswiki.org/highlight.el" :keywords '("faces" "help" "local"))
