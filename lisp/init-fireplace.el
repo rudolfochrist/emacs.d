dd458b4e424b0bd09dc38a442de413f71fb543c1
Sebastian Christ <rudolfo.christ@gmail.com>
2016-11-06 19:18:25 +0100

    having fun

--- lisp/init-fireplace.el
+++ lisp/init-fireplace.el
@@ -1,10 +1,28 @@
 (require-package 'fireplace)
 
+(defvar sexy-time-track "gaye.mp3"
+  "Sexy music to be played. 
+Provide the file name of the track.")
+
+(defvar sexy-time-buffer nil
+  "The sexy time music buffer.")
+
+(defvar sexy-time-buffer-name "*sexy-time*"
+  "The sexy time buffer name.")
+
 (defun sexy-time ()
   (interactive)
   (fireplace 1)
   (sleep-for 1)
-  (async-shell-command "afplay ~/Desktop/gaye.mp3")
+  (setq sexy-time-buffer (get-buffer-create sexy-time-buffer-name))
+  (let ((path (expand-file-name (format "~/.sexy-time/%s" sexy-time-track))))
+    (async-shell-command (format "afplay %s" path) sexy-time-buffer sexy-time-buffer))
   (delete-other-windows))
+
+(defun sexy-time-quit ()
+  (delete-process (get-process "Shell"))
+  (kill-buffer sexy-time-buffer))
+
+(advice-add 'fireplace-off :after #'sexy-time-quit)
 
 (provide 'init-fireplace)
