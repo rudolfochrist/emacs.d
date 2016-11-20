2660d6904372cd9ace3241fc1a7cf4b1c91c436f
Sebastian Christ <rudolfo.christ@gmail.com>
2016-11-20 12:54:50 +0100

    update packages

--- elpa/magithub-20161104.1621/magithub-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; magithub-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("magithub-cache.el" "magithub-ci.el" "magithub-core.el"
-;;;;;;  "magithub-issue.el" "magithub-pkg.el" "magithub.el") (22557
-;;;;;;  40530 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magithub-autoloads.el ends here
---
+++ elpa/magithub-20161105.1817/magithub-autoloads.el
@@ -0,0 +1,17 @@
+;;; magithub-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("magithub-cache.el" "magithub-ci.el" "magithub-core.el"
+;;;;;;  "magithub-issue.el" "magithub-pkg.el" "magithub.el") (22577
+;;;;;;  35997 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magithub-autoloads.el ends here
