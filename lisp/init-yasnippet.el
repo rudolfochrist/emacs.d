c31e8b0a28ec66eb057135d201ba9ff5448020e9
Sebastian Christ <rudolfo.christ@gmail.com>
2016-12-07 14:00:11 +0100

    No longer necessary because of yasnippet improvements.

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,21 +2,7 @@
 
 (yas-reload-all)
 
-(defun enable-yas-minor-mode ()
-  (yas-minor-mode))
-
-(add-hook 'prog-mode-hook 'enable-yas-minor-mode)
-
-;;; fix org-mode + yasnippet conflicts:
-;;; http://stackoverflow.com/questions/9418148/conflicts-between-org-mode-and-yasnippet
-(defun yas/org-very-safe-expand ()
-  (let ((yas/fallback-behavior 'return-nil))
-    (yas/expand)))
-(add-hook 'org-mode-hook (lambda ()
-                           (yas-minor-mode)
-                           (make-variable-buffer-local 'yas/trigger-key)
-                           (setq yas/trigger-key [tab}])
-                           (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-                           (define-key yas/keymap [tab] 'yas/next-field)))
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+(add-hook 'org-mode-hook #'yas-minor-mode)
 
 (provide 'init-yasnippet)
