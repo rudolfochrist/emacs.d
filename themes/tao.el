f598ebf246082868fcb056bc9861909317458be7
Sebastian Christ <rudolfo.christ@gmail.com>
2016-12-23 12:27:56 +0100

    some adjustments to tao theme.

--- themes/tao.el
+++ themes/tao.el
@@ -1,2 +1,6 @@
 (require-package 'tao-theme :noerror t :load-only t)
 (load-theme 'tao-yang t)
+
+(custom-set-faces
+ '(which-func ((t (:foreground "LightSkyBlue"))))
+ '(company-tooltip-common-selection ((t (:foreground "LightSkyBlue")))))
