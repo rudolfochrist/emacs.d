b0644fda0966489c829ab8b9ba4a68d42f040daa
Sebastian Christ <rudolfo.christ@gmail.com>
2016-12-25 22:05:49 +0100

    Theme adjustments.

--- themes/tao.el
+++ themes/tao.el
@@ -3,4 +3,8 @@
 
 (custom-set-faces
  '(which-func ((t (:foreground "LightSkyBlue"))))
- '(company-tooltip-common-selection ((t (:foreground "LightSkyBlue")))))
+ '(company-tooltip-common-selection ((t (:foreground "LightSkyBlue"))))
+ '(secondary-selection ((t (:background "LightSkyBlue"))))
+ '(org-level-1 ((t (:height 1))))
+ '(org-level-2 ((t (:height 1))))
+ '(org-level-3 ((t (:height 1)))))
