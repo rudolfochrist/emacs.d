c3f4a178a5132c8fd8e11c89af67704acdb55dc9
Sebastian Christ <rudolfo.christ@gmail.com>
2017-01-05 11:21:35 +0100

    Configure magit list repos.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -2,12 +2,12 @@
 (require-package 'magithub)
 (require-package 'git-timemachine)
 
-(setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+(setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
+      magit-repository-directories '(("~/.emacs.d/" . 0)
+                                     ("~/dev/" . 1)))
 
 ;; magit status
-(global-set-key (kbd "<f9>") 'magit-status)
-
-;;; git-timemachine
-(global-set-key (kbd "<M-f9>") 'git-timemachine)
+(global-set-key (kbd "<f9>") #'magit-status)
+(global-set-key (kbd "<M-f9>") #'magit-list-repositories)
 
 (provide 'init-magit)
