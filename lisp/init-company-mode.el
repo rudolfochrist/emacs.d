2ceb6b52d5d5f56a8dd6c94c0bf9e7aaae98e805
Sebastian Christ <rudolfo.christ@gmail.com>
2017-01-06 22:13:59 +0100

    Bind `company-complete' to force completion.
    Actually the readme suggest this. ¯\_(ツ)_/¯

--- lisp/init-company-mode.el
+++ lisp/init-company-mode.el
@@ -1,9 +1,12 @@
 (require-package 'company)
 
 (add-hook 'after-init-hook 'global-company-mode)
-(with-eval-after-load "company"
+(defun fyi-company-keybindings ()
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "M-.") 'company-show-location))
+(add-hook 'company-mode-hook #'fyi-company-keybindings)
+
+(global-set-key (kbd "C-' C-'") #'company-complete)
 
 (provide 'init-company-mode)
