1d0ad87e6d782d46edac5258a8ef2ca779082d2b
Sebastian Christ <rudolfo.christ@gmail.com>
2017-01-13 13:32:05 +0100

    New theme. Yay. Agan. /o\.

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -18,8 +18,7 @@
     (funcall mode -1)))
 
 ;;; theme du jour
-(load (expand-file-name "themes/tao" user-emacs-directory))
-
+(load (expand-file-name "themes/irreal" user-emacs-directory))
 
 ;; set font in GUI
 (when window-system
---
+++ themes/irreal.el
@@ -0,0 +1,3 @@
+;;; see http://irreal.org/blog/?p=3900
+(set-background-color "oldlace")
+(add-to-list 'default-frame-alist '(background-color . "oldlace"))
