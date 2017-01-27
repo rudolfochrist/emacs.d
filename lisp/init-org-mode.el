34f66622e9dc154df19a18c5dd05ea3c888326ae
Sebastian Christ <rudolfo.christ@gmail.com>
2017-01-27 10:20:10 +0100

    Use ox-texinfo and add references.

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -8,6 +8,7 @@
 (require 'ox-latex)
 (require 'ox-koma-letter)
 (require 'ox-md)
+(require 'ox-texinfo)
 (require 'org-checklist)
 
 ;;; Don't use cache to prevent arbitrary crashes
@@ -414,5 +415,54 @@ hypersetup to include colorlinks=true."
                 '("http" "https" "ftp" "mailto" "doi"))
       (format "%s \\footnote{\\url{%s}} " text (match-string 1 text)))))
 (add-to-list 'org-export-filter-link-functions #'org-export-latex-link-footnote)
+
+;;; texinfo export
+
+;;; use strike-through to markup vars in texinfo
+(add-to-list 'org-texinfo-text-markup-alist '(strike-through . "@var{%s}"))
+
+(defun org-texinfo-ref-open (path)
+  (let ((headline (org-find-exact-headline-in-buffer path (current-buffer) t)))
+    (if headline
+        (goto-char headline)
+      ;; try to find anchor
+      (let ((anchor (save-excursion
+                      (save-restriction
+                        (widen)
+                        (goto-char (point-min))
+                        (search-forward (format "@anchor{%s}" path) nil t)
+                        (when (match-string 0)
+                          (point))))))
+        (when anchor
+          (goto-char anchor))))))
+
+(defun org-texinfo-reference-export (ref-type path description backend)
+  (when (eql backend 'texinfo)
+    (format "@%s{%s}"
+            ref-type
+            (if description
+                (format "%s,,%s" path description)
+              path))))
+
+(defun org-texinfo-xref-export (path desc backend)
+  (org-texinfo-reference-export "xref" path desc backend))
+
+(org-link-set-parameters "texixref"
+                         :follow 'org-texinfo-ref-open
+                         :export 'org-texinfo-xref-export)
+
+(defun org-texinfo-ref-export (path desc backend)
+  (org-texinfo-reference-export "ref" path desc backend))
+
+(org-link-set-parameters "texiref"
+                         :follow 'org-texinfo-ref-open
+                         :export 'org-texinfo-ref-export)
+
+(defun org-texinfo-pxref-export (path desc backend)
+  (org-texinfo-reference-export "pxref" path desc backend))
+
+(org-link-set-parameters "texipxref"
+                         :follow 'org-texinfo-ref-open
+                         :export 'org-texinfo-pxref-export)
 
 (provide 'init-org-mode)
