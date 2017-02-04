1cbb0f249512bde7d9152aeb7ec6910f0f29aafe
Sebastian Christ <rudolfo.christ@gmail.com>
2017-02-04 21:08:07 +0100

    Remove old files.

--- lisp/init-goto-last-change.el
+++
@@ -1,5 +0,0 @@
-(require-package 'goto-last-change)
-
-(global-set-key (kbd "C-x C--") #'goto-last-change)
-
-(provide 'init-goto-last-change)
--- lisp/init-highlight-backquotes-mode.el
+++
@@ -1,5 +0,0 @@
-(require-package 'highlight-backquotes
-                 :from-dir
-                 (expand-file-name "highlight-backquotes-mode" emacs-d-site-lisp))
-
-(provide 'init-highlight-backquotes-mode)
