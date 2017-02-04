a858853262f756637119e889fb34fabdeb97b6a1
Sebastian Christ <rudolfo.christ@gmail.com>
2017-02-04 10:00:40 +0100

    Use use-package.

--- init.el
+++ init.el
@@ -2,10 +2,11 @@
 ;; use CL features
 (require 'cl-lib)
 
-;;; customization
-(setq emacs-d (expand-file-name "~/.emacs.d/")
-      emacs-d-site-lisp (expand-file-name "site-lisp/" emacs-d))
-(add-to-list 'load-path emacs-d-site-lisp)
+;;; site-lisp setup
+(setq  site-lisp-directory (expand-file-name "site-lisp/" user-emacs-directory))
+(add-to-list 'load-path site-lisp-directory)
+(let ((default-directory site-lisp-directory))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;;; prefer newer files
 (setq load-prefer-newer t)
@@ -19,12 +20,20 @@
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
+;;; setup use-package
+(require 'diminish)
+(require 'use-package)
+
+;;; Packages
+
 ;;; load other files
 ;;; - basics
 ;;; - packages
 ;;; - org
 ;;; - gnus
 
+
+;;; enable disabled commands
 (put 'erase-buffer 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'upcase-region 'disabled nil)
