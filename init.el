35888287cea5fcf519c93cb2abf3ec7b9e898a40
Sebastian Christ <schrist@ravenpack.com>
2017-08-21 09:02:51 +0200

    Load different color theme in Terminal.

--- init.el
+++ init.el
@@ -93,8 +93,11 @@
 
 ;;; default theme with custom background
 ;;; see http://irreal.org/blog/?p=3900
-(set-background-color "white smoke")
-(add-to-list 'default-frame-alist '(background-color . "white smoke"))
+;;; load a different theme in terminal
+(if (not (display-graphic-p))
+    (load-theme 'whiteboard t)
+  (set-background-color "white smoke")
+  (add-to-list 'default-frame-alist '(background-color . "white smoke")))
 
 ;;; make scripts executable if shebang present
 (add-hook 'after-save-hook
